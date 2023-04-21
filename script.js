69637c0a20e4b6896ffbdf21371e69a0b79eeb59
Romeo.exe <115535482+BornToBreackSecurity@users.noreply.github.com>
2023-04-21 12:07:26 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,71 +1,51 @@
-var countdownEl = document.getElementById("countdown");
-var startBtnEl = document.getElementById("startBtn");
+// Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyBqZZau44UfsioSuU2GFfavruiuv8k4Opo",
+  authDomain: "webfirebase-74a3f.firebaseapp.com",
+  projectId: "webfirebase-74a3f",
+  storageBucket: "webfirebase-74a3f.appspot.com",
+  messagingSenderId: "1013819772272",
+  appId: "1:1013819772272:web:2b377540bbe5d84a92d3b5",
+  measurementId: "G-EXQE821Y91"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
 
-var countdownTime = 5400000; // 1 hour 30 minutes in milliseconds
-var countDownDate;
+// Set the countdown date and time
+var countdownDate = new Date().getTime() + (1 * 60 * 60 * 1000) + (30 * 60 * 1000); // 1 hour 30 minutes from now
 
+// Define a variable to hold the countdown timer interval
+var countdownTimer;
+
+// Function to start the countdown timer
 function startCountdown() {
-  // Disable the "Start" button
-  startBtnEl.disabled = true;
-
-  // Get the current date and time
-  var now = new Date().getTime();
-
-  // Set the date and time when the countdown should end
-  countDownDate = now + countdownTime;
-
-  // Save the countdown end time in browser storage
-  localStorage.setItem("countDownDate", countDownDate);
-
   // Update the countdown every second
-  var x = setInterval(function() {
+  countdownTimer = setInterval(function() {
     // Get the current date and time
     var now = new Date().getTime();
-
-    // Find the distance between now and the countdown date
-    var distance = countDownDate - now;
-
-    // Time calculations for hours, minutes, and seconds
-    var hours = Math.floor(distance / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    // Display the countdown in the "countdown" paragraph
-    countdownEl.innerHTML = hours + "h " + minutes + "m " + seconds + "s ";
-
-    // Check if the countdown has expired
-    if (distance <= 0) {
-      clearInterval(x);
-      countdownEl.innerHTML = "EXPIRED";
-      startBtnEl.disabled = false;
-
-      // Redirect the user to a new page
-      window.location.href = "time_over.html";
-    }
+    
+    // Calculate the time remaining until the countdown date and time
+    var timeRemaining = countdownDate - now;
+    
+    // Calculate the hours, minutes, and seconds remaining
+    var hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+    
+    // Display the hours, minutes, and seconds remaining in the countdown timer
+    document.getElementById("countdown-hours").innerHTML = hours;
+    document.getElementById("countdown-minutes").innerHTML = minutes;
+    document.getElementById("countdown-seconds").innerHTML = seconds;
+    
+    // Store the countdown time in Firebase Realtime Database
+    database.ref("countdownTime").set({
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds
+    });
   }, 1000);
 }
 
-function loadCountdown() {
-  // Load the countdown end time from browser storage
-  var storedCountDownDate = localStorage.getItem("countDownDate");
-
-  if (storedCountDownDate) {
-    // Get the current date and time
-    var now = new Date().getTime();
-
-    // Find the distance between now and the stored countdown end time
-    var distance = storedCountDownDate - now;
-
-    // Check if the stored countdown end time has already passed
-    if (distance <= 0) {
-      countdownEl.innerHTML = "EXPIRED";
-      startBtnEl.disabled = false;
-    } else {
-      // Set the countdown end time to the stored value
-      countDownDate = storedCountDownDate;
-
-      // Start the countdown
-      startCountdown();
-    }
-  }
-}
+// Event listener for the start button
+document.getElementById("start-button").addEventListener("click", startCountdown);
