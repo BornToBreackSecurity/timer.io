8e424f178a70487a88b76ac1f16708813322bfd9
Romeo.exe <115535482+BornToBreackSecurity@users.noreply.github.com>
2023-04-22 13:14:10 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-     var countdownTime = 5400000;
+      var countdownTime = 5400000;
       var countdownEl = document.getElementById("countdown");
-      function startCountdown() {
-        document.querySelector("button").disabled = true;
         var now = new Date().getTime();
+
         var countDownDate = now + countdownTime;
+
         var x = setInterval(function() {
           var now = new Date().getTime();
+
           var distance = countDownDate - now;
+
           var hours = Math.floor(distance / (1000 * 60 * 60));
           var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
           var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
           countdownEl.innerHTML = hours + "h " + minutes + "m " + seconds + "s ";
+
           if (distance < 0) {
             clearInterval(x);
             countdownEl.innerHTML = "Expired";
           }
         }, 1000);
-      }
